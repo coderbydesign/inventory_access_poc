5d68bc4f6dc1af27e353e915f57d13cfae737097
Keith Walsh <kwalsh@redhat.com>
2023-12-11 10:37:07 -0500

    Mock implementation of /content/packages endpoint
    - Adds `Package` struct
    - Implements `VisitGetContentPackagesResponse` for the `GetContentPackagesResponseObject` interface on `Package`
    - Return mocked response
    
    TODO:
    - query from postgres
    - join with spicedb data
    - return limited results
    
    Local Data:
    I ran the following locally against my postgres container to load some data (see below for links):
    - cat test_generate_data.sql | docker exec -i <container> psql -U postgres -p 5434 -d content -W
    - cat test_data.sql | docker exec -i <container> psql -U postgres -p 5434 -d content -W
    - cat create_inventory_hosts.sql | docker exec -i <container> psql -U postgres -p 5434 -d content -W
    
    Data Files:
    - https://github.com/RedHatInsights/patchman-engine/blob/master/database_admin/schema/create_schema.sql
    - https://github.com/RedHatInsights/patchman-engine/tree/master/dev

--- server/server.go
+++ server/server.go
@@ -1,18 +1,52 @@
 package server
 
 import (
-	"context"
-	"github.com/authzed/authzed-go/v1"
-	"github.com/jackc/pgx/v5"
-	"github.com/merlante/inventory-access-poc/api"
+    "context"
+    "encoding/json"
+    "github.com/authzed/authzed-go/v1"
+    "github.com/jackc/pgx/v5"
+    "github.com/merlante/inventory-access-poc/api"
+    "net/http"
 )
 
 type ContentServer struct {
-	SpicedbClient *authzed.Client
-	PostgresConn  *pgx.Conn
+    SpicedbClient *authzed.Client
+    PostgresConn  *pgx.Conn
+}
+
+type PackageName struct {
+    Name    string `json:"name"`
+    Summary string `json:"summary"`
+}
+
+type Package struct {
+    NameId          int    `json:"name_id"`
+    Evra            string `json:"evra"`
+    DescriptionHash string `json:"description_hash"`
+    SummaryHash     string `json:"summary_hash"`
+    AdvisoryId      int    `json:"advisory_id"`
+    Synced          bool   `json:"synced"`
+}
+
+func (p Package) VisitGetContentPackagesResponse(w http.ResponseWriter) error {
+    jsonResponse, err := json.Marshal(p)
+    if err != nil {
+        return err
+    }
+
+    w.Write(jsonResponse)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
 func (*ContentServer) GetContentPackages(ctx context.Context, request api.GetContentPackagesRequestObject) (api.GetContentPackagesResponseObject, error) {
-	//TODO implement me
-	panic("implement me")
+    // Next:
+    // - query from postgres
+    // - join with spicedb data
+    // - return results
+    p := Package{NameId: 123, Evra: "1-2", DescriptionHash: "Testing", SummaryHash: "FooBar", AdvisoryId: 321, Synced: false}
+    return p, nil
 }
